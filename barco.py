e4686157b2820ca18d4d4a85a837e428620a185e
DOMINIC ISAI GARCIA BAUTISTA <dgarcia70@ucol.mx>
2024-11-20 16:28:24 -0600

    ya estaria, pero da error a la hora de hacer pulls

--- barco.py
+++ barco.py
@@ -1,8 +1,8 @@
 import pygame
 
 def cargar_barco():
-    barco = pygame.image.load("barquito.png")# Cargar el sprite del barco
+    barco = pygame.image.load("img/barquito4.png")# Cargar el sprite del barco
     barco = pygame.transform.scale(barco, (barco.get_width(), barco.get_height()))
     barco_rect = barco.get_rect()
-    barco_rect.center = (400, 228)
+    barco_rect.center = (376, 190)
     return barco, barco_rect
